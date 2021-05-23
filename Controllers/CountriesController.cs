b810fd3fdbcd5fa48f86b620175e3f1a98054402
Kiyota iMac <kiyotatakeshi0609@gmail.com>
2021-05-23 20:58:44 +0900

    Country 関連の GET の実装を追加

--- Controllers/CountriesController.cs
+++ Controllers/CountriesController.cs
@@ -1,4 +1,6 @@
+using System.Collections.Generic;
 using System.Linq;
+using BookApi.Dtos;
 using BookApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -15,11 +17,72 @@ namespace BookApi.Controllers
         }
 
         [HttpGet]
+        [ProducesResponseType(400)] // 必須ではない記述、ドキュメント的な役割
+        [ProducesResponseType(200, Type = typeof(IEnumerable<CountryDto>))] // 必須ではない記述、ドキュメント的な役割
         public IActionResult GetCountries()
         {
             var countries = _countryRepository.GetCountries().ToList();
 
-            return Ok(countries);
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var countriesDto = new List<CountryDto>();
+            foreach (var country in countries)
+            {
+                countriesDto.Add(new CountryDto{
+                    Id = country.Id,
+                    Name = country.Name
+                });
+            }
+            return Ok(countriesDto);
         }
+
+        // api/countries/countryId
+        [HttpGet("{countryId}")]
+        [ProducesResponseType(400)] // 必須ではない記述、ドキュメント的な役割
+        [ProducesResponseType(404)] // 必須ではない記述、ドキュメント的な役割
+        [ProducesResponseType(200, Type = typeof(CountryDto))] // 必須ではない記述、ドキュメント的な役割
+        public IActionResult GetCounty(int countryId)
+        {
+            if (!_countryRepository.CountryExists(countryId))
+                return NotFound();
+
+            var country = _countryRepository.GetCountry(countryId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var countryDto = new CountryDto()
+            {
+                Id = country.Id,
+                Name = country.Name
+            };
+
+            return Ok(countryDto);
+        }
+
+        // api/countries/authors/authorId
+        [HttpGet("authors/{authorId}")]
+        [ProducesResponseType(400)] // 必須ではない記述、ドキュメント的な役割
+        [ProducesResponseType(404)] // 必須ではない記述、ドキュメント的な役割
+        [ProducesResponseType(200, Type = typeof(CountryDto))] // 必須ではない記述、ドキュメント的な役割
+        public IActionResult GetCountyOfAnAuthor(int authorId)
+        {
+            // TODO: Validate the author exists
+            var country = _countryRepository.GetCountryOfAnAuthor(authorId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var countryDto = new CountryDto()
+            {
+                Id = country.Id,
+                Name = country.Name
+            };
+
+            return Ok(countryDto);
+        }
+
+        // TODO: GetAuthorsFromCountry
     }
 }
